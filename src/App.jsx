76e7df48cf566b72fa81aee7e54cab83e54475ce
Shubhanshu Mohanty <shubhanshumohanty@gmail.com>
2024-02-02 12:05:20 +0530

    componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/Navbar'
+import UserDetails from './components/UserDetails'
+// import DeleteAllUser from './components/DeleteAllUser'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+    <Navbar/>
+    <UserDetails/>
+    {/* <DeleteAllUser/> */}
     </>
   )
 }
