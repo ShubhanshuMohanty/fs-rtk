ffaaa17f322f9864c7b4b0cdc4440cbfff3d3aec
Shubhanshu Mohanty <shubhanshumohanty@gmail.com>
2024-02-02 20:06:38 +0530

    access the state data

--- src/store/slice/UserSlice.jsx
+++ src/store/slice/UserSlice.jsx
@@ -4,7 +4,9 @@ const userSlice = createSlice({
   name: "user",
   initialState: [],
   reducers: {
-    addUser(state, action) {},
+    addUser(state, action) {
+      state.push(action.payload)
+    },
     removeUser(state, action) {},
     deleteUsers(state, action) {},
   },
@@ -13,3 +15,5 @@ const userSlice = createSlice({
 console.log(userSlice.actions);
 
 export default userSlice.reducer;
+
+export const {addUser}=userSlice.actions;
